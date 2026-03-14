2df576fe1a3b3a0a7092568ff19592941daa90fd
DataKumpel <tmbenkenstein@gmail.com>
2026-03-14 16:55:34 +0100

    Added camera module

---
+++ engine_core/src/camera.rs
@@ -0,0 +1,52 @@
+use bytemuck::{Pod, Zeroable};
+use glam::{Mat4, Vec3};
+
+
+// ---> Correction matrix for WGPU (z in range 0 to 1), OpenGL (z in range -1 to 1)
+pub const OGL_2_WGPU: Mat4 = Mat4::from_cols_array(&[
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0, 
+    0.0, 0.0, 0.5, 0.0, 
+    0.0, 0.0, 0.5, 1.0,
+]);
+
+
+//===== CAMERA STRUCTURE =====//
+pub struct Camera {
+    pub eye: Vec3,
+    pub target: Vec3,
+    pub up: Vec3,
+    pub aspect: f32,
+    pub fovy: f32,
+    pub clip_near: f32,
+    pub clip_far: f32,
+}
+
+impl Camera {
+    pub fn build_view_projection_matrix(&self) -> Mat4 {
+        let view = Mat4::look_at_rh(self.eye, self.target, self.up);
+        let proj = Mat4::perspective_rh(self.fovy, self.aspect, self.clip_near, self.clip_far);
+        OGL_2_WGPU * proj * view
+    }
+}
+//===== CAMERA STRUCTURE =====//
+
+
+//===== CAMERA UNIFORM STRUCTURE =====//
+#[repr(C)]
+#[derive(Debug, Clone, Copy, Pod, Zeroable)]
+pub struct CameraUniform {
+    pub view_proj: [f32; 4*4],
+}
+
+impl CameraUniform {
+    pub fn new() -> Self {
+        Self { view_proj: Mat4::IDENTITY.to_cols_array() }
+    }
+
+    pub fn update_view_proj(&mut self, camera: &Camera) {
+        self.view_proj = camera.build_view_projection_matrix().to_cols_array();
+    }
+}
+//===== CAMERA UNIFORM STRUCTURE =====//
+
