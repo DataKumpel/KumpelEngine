9c03aa0317af6bc7ffeae0a8156fa7d1baf21051
DataKumpel <tmbenkenstein@gmail.com>
2026-03-20 20:52:13 +0100

    Added light radius as 4th entry in light uniform position

--- engine_core/src/light.rs
+++ engine_core/src/light.rs
@@ -1,21 +1,21 @@
 use bytemuck::{Pod, Zeroable};
 
 
-//===== LIGHT UNIFORM STRUCTURE =====//
+//***** LIGHT UNIFORM STRUCTURE *******************************************************************
 #[repr(C)]
 #[derive(Debug, Clone, Copy, Pod, Zeroable)]
 pub struct LightUniform {
-    pub position: [f32; 4], // xyz + 1 x padding
+    pub position: [f32; 4], // xyz + radius
     pub color: [f32; 4],    // rgba
 }
 
 impl LightUniform {
-    pub fn new(position: [f32; 3], color: [f32; 3]) -> Self {
+    pub fn new(position: [f32; 3], color: [f32; 3], radius: f32) -> Self {
         Self { 
-            position: [position[0], position[1], position[2], 1.0], 
+            position: [position[0], position[1], position[2], radius], 
             color: [color[0], color[1], color[2], 1.0], 
         }
     }
 }
-//===== LIGHT UNIFORM STRUCTURE =====//
+//***** LIGHT UNIFORM STRUCTURE *******************************************************************
 
